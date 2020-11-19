ed7b8917deaa84de0e36d66a93819c4c9f00745b
nakamura1111 <5040.armuakan@gmail.com>
2020-11-19 16:51:29 +0900

    配布用exeファイルの作成

--- load_image.py
+++ load_image.py
@@ -1,10 +1,19 @@
 import os.path
 import pygame
+import sys
 
-main_dir = os.path.split(os.path.abspath(__file__))[0]
+# exeファイル作成時の参照先を変化に対応する
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+# main_dir = os.path.split(os.path.abspath(__file__))[0]
 
 def load_image(file):
-  file = os.path.join(main_dir, 'data', file)
+  # file = os.path.join(main_dir, 'data', file)
+  relative_path = os.path.join('data', file)
+  file = resource_path(relative_path)
   try:
     surface = pygame.image.load(file)
   except pygame.error:
